f44d336075a676bac455594f248608374f8d075e
Rimaben Patel <99620797+rima0055@users.noreply.github.com>
2024-01-10 14:03:03 -0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,16 +8,31 @@ function Work() {
 
       <div className="job">
         <p className="job__title">Front-end Developer</p>
-        <p className="company__name">Akash Technolabs | June 2018 - Januray 2019</p>
+        <p className="company__name">DIT Web Solutions Inc | Octember 2023 - December 2023</p>
         <ul className="job__points">
+          <li>Creating the website architecture.</li>
+          <li>Designing and managing the website back-end including database and server integration.</li>
+	        <li>Generating WordPress themes and plugins.</li>
+          <li>Planning and creating the website architecture.</li>
+          <li>Establishing and guiding website architecture so that sites are attractive and user-friendly.</li>
+          <li>Troubleshooting and resolving website problems for clients and co-workers.</li>
           <li>Developed user interfaces for web applications using HTML, CSS, and JavaScript.</li>
-          <li>Analyzed customer requirements to create efficient software solutions.</li>
-          <li>Tested front-end code in multiple browsers to ensure cross-browser compatibility.</li>
+          <li>Designing and building the website front-end.</li>
+          <li>Created successful websites that met requirements for objectives such as load speed and design.</li>
+        </ul>
+      </div>
+
+      <div className="job">
+        <p className="job__title">Software Developer</p>
+        <p className="company__name">Akash Technolabs | June 2018 - February 2019</p>
+        <ul className="job__points">
+          <li>Got hands-on experience in professional software engineering practices & best practices for the full software development life cycle, including coding standards, code reviews, source control management, build processes, testing, and operations.</li>
+        	<li>Design, develop, modify, debug and/or maintain software code according to functional, non-functional, and technical design specifications.</li>
           <li>Proficient in using Git for version control, merging and branching.</li>
-          <li>Collaborated with back-end developers to integrate user-facing elements with server-side logic.</li>
-          <li>Developed site content and graphics by coordinating with copywriters and graphic artists.</li>
-          <li>Utilized Java and OOP languages throughout development phases.</li>
-          <li>Utilizing web applications and programming languages</li>
+          <li>Collaborated with cross-functional teams to identify and solve complex software development problems.</li>
+	        <li>Implemented security measures like authentication and authorization using Spring Security.</li>
+          <li>Experience in working with multi-threading and concurrency in Java.</li>
+         	<li>Worked in an agile environment to deliver high-quality software.</li>
         </ul>
       </div>
     </div>
